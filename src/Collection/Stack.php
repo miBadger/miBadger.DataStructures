3bfacf2f0c9d459398d13d32db62694c7a5ccf50
markzijp <Megazijp@gmail.com>
2018-04-12 12:37:36 +0200

    comment fix

--- src/Collection/Stack.php
+++ src/Collection/Stack.php
@@ -24,88 +24,88 @@ class Stack implements \IteratorAggregate, CollectionInterface
 	}
 
 	/**
-    * Replaces the stack with a custom array
-    *
-    * @return Stack returns itself
-    */
+     * Replaces the stack with a custom array
+     *
+     * @return Stack returns itself
+     */
 	public function replaceData($array){
 		$this->stack = $array;
 		return $this;
 	}
 
 	/**
-    * Pushes an item onto the top of this stack
-    *
-    * @return void
-    */
+     * Pushes an item onto the top of this stack
+     *
+     * @return void
+     */
 	public function push($item){
 		array_push($this->stack, $item);
 	}
 
 	/**
-    * Removes the object at the top of the stack and returns that object
-    *
-    * @return mixed the object on top of the stack
-    */
+     * Removes the object at the top of the stack and returns that object
+     *
+     * @return mixed the object on top of the stack
+     */
 	public function pop(){
 		return array_pop($this->stack);
 	}
 
 	/**
-    * Looks at the object at the top of this stack without removing it from the stack
-    *
-    * @return mixed the object at the top of this stack
-    */
+     * Looks at the object at the top of this stack without removing it from the stack
+     *
+     * @return mixed the object at the top of this stack
+     */
 	public function peek(){
 		return end($this->stack);
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function isEmpty(){
 		return $this->stack.isEmpty();
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function toArray(){
 		return $this->stack;
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function copy(){
 		return (new Stack())->replaceData($this->stack);
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function size(){
 		return size($this->stack);
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function clear(){
 		unset($this->stack);
 		$this->stack = array();
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function count(){
 		return count($this->stack);
 	}
 
 	/**
-    * @inherit
-    */
+     * @inherit
+     */
 	public function getIterator(){
 		return new \ArrayIterator($this->stack);
 	}
